04aee6a2c6bca8e035f2259aa8ac68b8132f1daf
unknown <atharvasadaphale22@gmail.com>
2023-01-13 14:23:02 +0530

    sol 3 poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,13 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Product p= new Product();
         int res1=p.product(5,6);
-        System.out.println(res1); // prints 30
 
         int res2 = p.product(5, 6, 7);
-        System.out.println(res2); // prints 210
 
         double res3 = p.product(5.5, 6.5);
-        System.out.println(res3);
     }
     static class Product{
         public int product(int x, int y) {
